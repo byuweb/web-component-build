683fbc37e2668bd55fe4f691387ee2eefdca86ed
Aaron Rasmussen <arasmus8@gmail.com>
2018-05-11 11:38:45 -0600

    simple rollup bundler experiment 5/11/18 crazy friday

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /*
  *  @license
  *    Copyright 2017 Brigham Young University
@@ -14,8 +15,55 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-"use strict";
+'use strict'
 
-module.exports = {
-    gulp: require('./runner/gulp'),
-};
+const path = require('path')
+const fs = require('fs-extra')
+const config = {}
+/* require(path.resolve(projectRoot, 'web-component-build-config.js')) // TODO make config file in project root and it should be optional */
+const rollup = require('rollup')
+
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+const inputOptions = {
+  plugins: [
+    resolve({}),
+    commonjs({
+      // non-CommonJS modules will be ignored, but you can also
+      // specifically include/exclude files
+      include: 'node_modules/**' // Default: undefined
+    })
+  ]
+
+}
+const outputOptions = {
+  format: 'es'
+}
+
+async function buildComponents ({sourceDir = 'components', destDir = 'dist'}) {
+  console.log(`Building Components in ${sourceDir}...`)
+
+  // get all .js files in sorceDir
+  const fileContents = await fs.readdir(path.resolve(proccess.cwd(), sourceDir))
+  const sourceFiles = fileContents
+    .filter(f => path.parse(f).ext === 'js')
+    .filter(async f => {
+      const stats = await fs.stat(f)
+      return stats.isFile()
+    })
+
+  // do rollup on them
+  const rollupPromises = sourceFiles.map(async (f, i) => {
+    const {name, ext} = path.parse(f)
+    console.log(`Building (${i} of ${sourceFiles.length}) ${f}...`)
+    const bundle = await rollup.rollup({input: f, ...inputOptions})
+    return bundle.write({file: path.resolve(proccess.cwd(), destDir, name + '-bundle' + ext), ...outputOptions})
+  })
+  await rollupPromises
+
+  console.log('Build Finished!')
+
+  // TODO make an optional es5 bundle as well
+}
+
+buildComponents(config).then(() => console.log('done'))
