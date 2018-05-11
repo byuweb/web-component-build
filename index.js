061b513215c627bd75356529e0f910204f382e4c
Aaron Rasmussen <arasmus8@gmail.com>
2018-05-11 16:15:32 -0600

    working mvp

--- index.js
+++ index.js
@@ -40,30 +40,55 @@ const outputOptions = {
   format: 'es'
 }
 
+const reportError = err => {
+  console.error('Error in build process:')
+  console.error(err)
+}
+
 async function buildComponents ({sourceDir = 'components', destDir = 'dist'}) {
   console.log(`Building Components in ${sourceDir}...`)
 
   // get all .js files in sorceDir
-  const fileContents = await fs.readdir(path.resolve(proccess.cwd(), sourceDir))
+  const fileContents = await fs.readdir(path.resolve(process.cwd(), sourceDir))
   const sourceFiles = fileContents
-    .filter(f => path.parse(f).ext === 'js')
-    .filter(async f => {
+  .filter(f => path.extname(f) === '.js')
+  .map(f => path.resolve(process.cwd(), sourceDir, f))
+  .filter(async f => {
+    try {
       const stats = await fs.stat(f)
       return stats.isFile()
-    })
-
-  // do rollup on them
-  const rollupPromises = sourceFiles.map(async (f, i) => {
-    const {name, ext} = path.parse(f)
-    console.log(`Building (${i} of ${sourceFiles.length}) ${f}...`)
-    const bundle = await rollup.rollup({input: f, ...inputOptions})
-    return bundle.write({file: path.resolve(proccess.cwd(), destDir, name + '-bundle' + ext), ...outputOptions})
+    } catch (err) {
+      reportError(err)
+      return false
+    }
   })
-  await rollupPromises
+
+  // call rollup on each of them
+  const rollupPromises = sourceFiles.map(async (f, i) => {
+    try {
+      const {name, ext} = path.parse(f)
+      console.log(`Building (${i+1} of ${sourceFiles.length}) ${f}...`)
+      const fullInputOptions = Object.assign({}, {input: f}, inputOptions)
+      const bundle = await rollup.rollup(fullInputOptions)
+      const destFilename = path.resolve(process.cwd(), destDir, name + '-bundle' + ext)
+      const fullOutputOptions = Object.assign({}, {file: destFilename}, outputOptions)
+      return bundle.write(fullOutputOptions)
+    } catch (err) {
+      reportError(err)
+    }
+  })
+
+  try {
+    await rollupPromises
+  } catch (err) {
+    reportError(err)
+  }
 
   console.log('Build Finished!')
 
   // TODO make an optional es5 bundle as well
 }
 
-buildComponents(config).then(() => console.log('done'))
+buildComponents(config)
+.then(() => console.log('done'))
+.catch(err => reportError(err))
